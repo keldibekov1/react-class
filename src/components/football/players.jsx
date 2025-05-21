0929c8ad80ab33e55e094b9f58c3e0a0c3ff54a3
O'tkir <keldibekovotkir767@gmail.com>
2025-05-21 08:39:18 +0500

    full

--- src/components/football/players.jsx
+++ src/components/football/players.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Player from "./player";
+import Player from "./Player";
 
 const Players = ({ players, deletePlayer }) => (
   <div className="max-w-4xl mx-auto mt-10">
