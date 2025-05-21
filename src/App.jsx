b6ad6d62011540514fed407a45bdae9e91cbcb84
O'tkir <keldibekovotkir767@gmail.com>
2025-05-21 08:35:15 +0500

    full

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
-import Header from './components/header/Header'
-import Food from './components/food/Food'
-import Form from './components/form/Form'
-import Country from './components/country/Country'
 import PlayerForm from './components/football/playerForm'
 
 const App = () => {
   return (
     <div >
-      {/* <Header/>
-      <h2>Class component</h2>
-      <Food/> */}
-       {/* <Form/> */}
+    
          <PlayerForm/>
         
 
